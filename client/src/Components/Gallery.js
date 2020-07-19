c4b49c4b267a9e5b9f31f11ee64bd2d630572394
802ScubaFish <christianrdow@gmail.com>
2020-07-19 12:52:46 -0400

    updated image gallery, addeded addition styling

--- client/src/Components/Gallery.js
+++ client/src/Components/Gallery.js
@@ -1,18 +1,52 @@
 import React, { useState } from 'react';
 // Material ui Imports
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from "@material-ui/core/IconButton";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 // 
+// Image Import List
+import imageOne from '../Images/DSC_0075.jpg';
+import imageTwo from '../Images/DSC_0283.jpg';
+import imageThree from '../Images/DSC_0032.jpg';
+import imageFour from '../Images/DSC_0103.jpg';
+import imageFive from '../Images/DSC_0113.jpg';
+import imageSix from '../Images/DSC_0169.jpg';
+import imageSeven from '../Images/DSC_0208.jpg';
+import imageEight from '../Images/DSC_0247.jpg';
+import imageNine from '../Images/DSC_0309.jpg';
+import imageTen from '../Images/DSC_0313.jpg';
+import imageEleven from '../Images/DSC_0315.jpg';
+import imageTwelve from '../Images/DSC_0301.jpg';
+import imageThirteen from '../Images/DSC_0341-Edit.jpg';
+import imageFourteen from '../Images/DSC_0641.jpg';
+
 
 // The Array of Images
 const imgArray = [
-    'https://giecdn.azureedge.net/storage/fileuploads/image/2019/09/25/rainwater_draining_off_roof-adobe_stock-credit-igor_batenev-resized.jpg?w=736&h=414&mode=crop',
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTysQtavhuwbdAnxm1iUC_KRGFJXgai7ZlTuw&usqp=CAU",
-    "https://cdn.videvo.net/videvo_files/images/preview_Raindrops_Videvo.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDtClqMeLUk8Os1l79htYIBVHxE-cGV563CQ&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzLRbGuRadXmx4qeApX-3Zdu9mcNL7wVla3w&usqp=CAU",
+    imageOne,
+    imageTwo,
+    imageThree,
+    imageFour,
+    imageFive,
+    imageSix,
+    imageSeven,
+    imageEight,
+    imageNine,
+    imageTen,
+    imageEleven,
+    imageTwelve,
+    imageThirteen,
+    imageFourteen
 ]
+
+const theme = createMuiTheme({
+    palette: {
+        secondary: {
+            main: '#00CED1'
+        }
+    }
+});
 
 
 function Gallery() {
@@ -33,18 +67,19 @@ function Gallery() {
 
     return (
         <div className="App">
+            <h3 id='galleryHead'>A small Gallery built with <span id='turqColor'>React</span> including some of my Personal Images that have been distrubuted by companies such as Subaru of America*</h3>
             <div id="rotating-img-carousel">
 
                 <div id="img-carousel">
                     <img
                         className="xs-img-left"
-                        src={index === 1 ? imgArray[imgArray.length - 1] : index === 0 ? imgArray[imgArray.length - 2] : imgArray[index - 2]}
-                        alt="sm-img"
+                        src={index === imgArray.length - 2 ? imgArray[0] : index === imgArray.length - 1 ? imgArray[1] : imgArray[index + 2]}
+                        alt="xs-img"
                     />
 
                     <img
                         className="sm-img-left"
-                        src={index === 0 ? imgArray[imgArray.length - 1] : imgArray[index - 1]}
+                        src={index === imgArray.length - 1 ? imgArray[0] : imgArray[index + 1]}
                         alt="sm-img"
                     />
 
@@ -52,28 +87,30 @@ function Gallery() {
 
                     <img
                         className="sm-img-right"
-                        src={index === imgArray.length - 1 ? imgArray[0] : imgArray[index + 1]}
+                        src={index === 0 ? imgArray[imgArray.length - 1] : imgArray[index - 1]}
                         alt="sm-img"
                     />
 
                     <img
                         className="xs-img-right"
-                        src={index === imgArray.length - 2 ? imgArray[0] : index === imgArray.length - 1 ? imgArray[1] : imgArray[index + 2]}
-                        alt="sm-img"
+                        src={index === 1 ? imgArray[imgArray.length - 1] : index === 0 ? imgArray[imgArray.length - 2] : imgArray[index - 2]}
+                        alt="xs-img"
                     />
                 </div>
 
             </div>
             <div id='nav-buttons'>
-                <IconButton
-                    onClick={() => { handleNav("left"); }} >
-                    <ArrowBackIosIcon fontSize="large" />
-                </IconButton>
-
-                <IconButton
-                    onClick={() => { handleNav("right"); }} >
-                    <ArrowForwardIosIcon fontSize="large" />
-                </IconButton>
+                <MuiThemeProvider theme={theme}>
+                    <IconButton
+                        onClick={() => { handleNav("left"); }} >
+                        <ArrowBackIcon color='secondary' fontSize="large" />
+                    </IconButton>
+                    
+                    <IconButton theme={theme}
+                        onClick={() => { handleNav("right"); }} >
+                        <ArrowForwardIcon color='secondary' fontSize="large" />
+                    </IconButton>
+                    </MuiThemeProvider>
             </div>
         </div>
     );
