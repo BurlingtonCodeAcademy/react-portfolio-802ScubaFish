27da48534c4d63801882604312807567272d158a
802ScubaFish <christianrdow@gmail.com>
2020-09-08 13:54:18 -0400

    had free time, working on mobile view corrections

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -15,19 +15,19 @@ function Projects() {
             <h1>Current <span id='turqColor'> Projects</span></h1>
 
             {/* Main Page Sub-Title */}
-            <h4>Linked to Github Repositories</h4>
+            <h5>*If Not Currently Deployed than Linked to Github Repositories</h5>
 
             {/* The Top Row */}
             {/* Find Your Fun! Project Here */}
             <div className='rows'>
                 <div className='halfWidth pushImg'>
                     <img className='projectImg' src={findYourFun} alt="Project Screen Shot" />
-                    <a className='projectLinks' href='https://whispering-beach-64323.herokuapp.com/'><h3 class='center'>Find Your Fun - An App Created for -<span id="green">Vermont Parks and Recreation</span></h3><h5 class='center'>A Mobile Friendly Source of Events and Facilities in Vermont!</h5></a>
+                    <a className='projectLinks' href='https://whispering-beach-64323.herokuapp.com/'><h3 class='center'>Find Your Fun!</h3><h5 class='center' id='green'>An App Created in Partnership with Vermont Parks and Recreation Association</h5><h5 class='center'>A Mobile Friendly Source of Events and Facilities in Vermont!</h5></a>
                 </div>
             {/* The Fox Game Project Here */}
                 <div className='halfWidth'>
                     <img className='projectImg' src={foxGame} alt="Project Screen Shot" />
-                    <a className='projectLinks' href='https://enigmatic-savannah-53571.herokuapp.com/'><h3 class='center'>The Fox Game!</h3><h5 class='center'>Similar to The Classic 'Tamagotchi'</h5></a>
+                    <a className='projectLinks' href='https://enigmatic-savannah-53571.herokuapp.com/'><h3 class='center'>The Fox Game!</h3><h5 class='center'>Similar to The Classic 'Tamagotchi' Game</h5><h5 class='center'>Created through Frontend Master Classes</h5></a>
                 </div>
             </div>
 
@@ -36,7 +36,7 @@ function Projects() {
             <div className='rows'>
                 <div className='halfWidth pushImg'>
                     <img className='projectImg' src={zorkingtonSS} alt="Project Screen Shot" />
-                    <a className='projectLinks' href='https://github.com/802ScubaFish/zorkington-gabe-christian'><h3 class='center'>Zorkington - Pokemon Edition</h3><h5 class='center'>Pokemon Themed Zorkington!</h5></a>
+                    <a className='projectLinks' href='https://github.com/802ScubaFish/zorkington-gabe-christian'><h3 class='center'>Zorkington Pokemon Edition</h3><h5 class='center'>A Pokemon Themed Choose Your Own Adventure!</h5></a>
                 </div>
             {/* Yelpington Project Here */}
                 <div className='halfWidth'>
@@ -50,7 +50,7 @@ function Projects() {
             <div className='rows'>
                 <div className='halfWidth pushImg'>
                 <img className='projectImg' src={remockSS} alt="Project Screen Shot" />
-                <a className='projectLinks' href= 'https://github.com/802ScubaFish/remock-802ScubaFish'><h3 class='center'>Remok Website</h3><h5 class='center'>A Recreation of a Website</h5></a>
+                <a className='projectLinks' href= 'https://github.com/802ScubaFish/remock-802ScubaFish'><h3 class='center'>Remok Website</h3><h5 class='center'>A Recreation of Website Layout Photos</h5></a>
                 </div>
             {/* Tic Tac Toe Project Here */}
                 <div className='halfWidth'>
