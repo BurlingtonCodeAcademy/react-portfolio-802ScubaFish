0e2e8d167ea77fce0a861c5e2ab0221656407092
802ScubaFish <christianrdow@gmail.com>
2021-03-03 15:10:18 -0500

    new mobile menu and major UX imporvements

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect }from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 // This is the Navigation Bar Component That exists on all of the pages
 function NavBar() {
+
+    const [modalState, setModalState] = useState('none');
+
+    const [xState, setXState] = useState('');
+
+    const location = useLocation();
+
+    useEffect(() =>{
+        setXState('')
+        setModalState('none')
+    }, [location])
+
+    function xHandler() {
+
+        if (xState === '') {
+            setXState('open')
+            setModalState('flex')
+        } else {
+            setXState('')
+            setModalState('none')
+        }
+    }
+
     return (
+        <>
             <nav id='navBar'>
                 {/* This is the Left Side which links back to the main page from any location if clicked */}
                 <div id='alignLeft'>
@@ -20,7 +44,32 @@ function NavBar() {
                     <Link className='navBarText' to='/gallery'>Gallery</Link>
                 </div>
             </nav>
+            <div id='hamNavSection'>
+            {/* Hamburger menu / "Bar icon" from FontAwesome to toggle the navigation links */}
+                
+                <div id='alignLeft'>
+                    <Link to="/" className='navBarText'> Christian <span id='lastName'>Dow</span></Link>
+                </div>
+                
+                <div onClick={xHandler} id="nav-icon2" className={xState}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div id="hamModal" style={{display: modalState}}>
+                <Link className='navBarHomeText' to='/'>Home</Link>
+                <Link className='navBarText' to='/about'>About</Link>
+                <Link className='navBarText' to='/projects'>Projects</Link>
+                <Link className='navBarText' to='/experience'>Experience</Link>
+                <Link className='navBarText' to='/gallery'>Gallery</Link>
+            </div>
+        </>
     )
 }
 
 export default NavBar;
+
