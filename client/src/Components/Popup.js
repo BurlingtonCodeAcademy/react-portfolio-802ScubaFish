280bb7d607d3d23bf2d998888c59d3fb91a9f706
802ScubaFish <christianrdow@gmail.com>
2021-04-08 14:06:55 -0400

    typo!

--- client/src/Components/Popup.js
+++ client/src/Components/Popup.js
@@ -25,9 +25,9 @@ export default function Popup() {
     return (
         <div className='popup' style={{display: popupState}}>
         <h2>Welcome!</h2>
-           <h3>It looks like this is your <span id='turqColor'>1st</span> Visit to my Site!</h3>
+           <h3>It looks like this is your <span id='turqColor'>1st</span> Visit to My Site!</h3>
            <h4>This website is written entirely with <span id='turqColor'>React</span> (including HTML, Javascript, CSS & More!)</h4>
-           <h4>I hope you enjoy you visit! <span role='img' aria-label='celebrate'>🎉</span></h4>
+           <h4>I hope you enjoy your visit! <span role='img' aria-label='celebrate'>🎉</span></h4>
            <h5>If you want to skip right to contacting me directly</h5>
            <Link className='contactMeLinkPopup' id='center' to='/contact-me'>Fill out this form!</Link>
            <button onClick={closeHandler} id='popupButton'>Close</button>
